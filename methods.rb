b79adba3438b23969ceef661b149ee8249b0adc4
Bernardngari <bernardngari.ke@gmail.com>
2022-08-15 21:22:45 +0300

    commit tree b287d200cee5eb434b707743b8d609880dfa819c

--- methods.rb
+++ methods.rb
@@ -1,1 +1,14 @@
 # Your code here!
+def greet_programmer
+	puts "Hello, programmer!"
+end
+
+def greet name
+	puts "Hello, #{name}!"
+end
+
+def greet_with_default (name="programmer")
+	puts "Hello, #{name}!".
+end
+
+#
